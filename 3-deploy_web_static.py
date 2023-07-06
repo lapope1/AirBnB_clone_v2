9c527dcdce428af6c198dd463629f92751db0fb2
Ayomiposi Popoola <ayomiposi4joy@gmail.com>
2023-07-06 13:46:10 -0400

    Update 3-deploy_web_static.py

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -6,7 +6,7 @@ from fabric.api import local
 from fabric.api import put
 from fabric.api import run
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['34.207.227.3', '100.25.22.97']
 
 
 def do_pack():
