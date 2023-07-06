36e1d9430975428aae7a4399eb0cb962d08f679b
Okene4000 <117732316+Okene4000@users.noreply.github.com>
2023-07-06 16:02:42 +0100

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['54.82.199.97', '52.3.240.168']
+env.hosts = ['100.25.19.204', '54.157.159.85']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
